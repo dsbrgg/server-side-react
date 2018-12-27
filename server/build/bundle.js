5eb48ef1473630f36771245364eab9275eb880c8
Diego Braga <dsbrgg@gmail.com>
2018-12-27 18:01:19 +0000

    express redirects all paths to React

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -90,7 +90,7 @@ var app = (0, _express2.default)();
 
 app.use(_express2.default.static('public'));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.send((0, _renderer2.default)(req));
 });
 
