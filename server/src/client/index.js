ea4e859b37d3dec776dc7a2287392742fcd2214a
Diego Braga <dsbrgg@gmail.com>
2018-12-29 17:26:47 +0000

    setup axios instance for the client and add redux thunk support

--- server/src/client/index.js
+++ server/src/client/index.js
@@ -9,10 +9,20 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
+import axios from 'axios'
+
 import Routes from './Routes'
 import reducers from './reducers'
 
-const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk))
+const api = axios.create({
+  baseURL: '/api'
+})
+
+const store = createStore(
+  reducers, 
+  window.INITIAL_STATE, 
+  applyMiddleware(thunk.withExtraArgument(api))
+)
 
 ReactDOM.hydrate(
   <Provider store={store}>
